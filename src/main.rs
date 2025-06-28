1452c7ad0fec64edb753f4fe578d26dfe1952790
Maximilian Beier <beiermaximilian13@gmail.com>
2025-06-28 16:46:40 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,5 @@
-
-#[derive(Default)]
-struct Task {
-    description: String,
-    is_done: Option<bool>
-}
-
-struct Tasks {
-    tasks: Vec<Task>
-}
-
-impl Tasks {
-    fn new() -> Self{
-        Tasks { tasks: Vec::new() }
-    }
-
-    fn add_task(&mut self, task: Task) {
-        self.tasks.push(task);
-
-    }
-
-    fn get_task(&mut self, pos: usize) -> Option<&mut Task> {
-        return self.tasks.get_mut(pos - 1)
-    }
-    
-    fn list_tasks(&self){
-        for (index, task) in self.tasks.iter().enumerate(){
-            match task.is_done{
-                Some(true) => println!("[x] Task {} is {}", index + 1, task.description),
-                Some(false) | None => println!("[ ] Task {} is {}", index + 1, task.description)
-            }
-        }
-    }
-}
+mod task;
+use task::{Task, Tasks};
 
 fn main() {
     let task1 = Task{
